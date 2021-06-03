cabf957d432f060291e6899fffa6718b0ee15ede
ksp1510 <42215711+ksp1510@users.noreply.github.com>
2021-06-03 23:18:45 +0530

    lot of changes done new function created to find route using start and end stations.

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ Assignment 1
 """
 
 import train
+import trains
 
-print(train.calcDistance(["A","B","E","F"]))
-# print(train.isRoute(["A","B","E","F"]))
 
+#print(train.calcDistance(["B"))
+#print(train.calcDistance.__annotations__)
+#print(train.findRoute("A","C"))
+#print(train.calcDistance(trains.shortestRoute("A","C")))
+print(trains.shortestRoute("A","C"))
--- train.py
+++ train.py
@@ -1,53 +1,36 @@
 # Implementation of graph using dictionary data type.
+
+
 # Below is the implementation of graph using dict Keys are starting stations node and list values are stations directly
 # linked to the starting station along with distance between them.
 stations_routes = {"A": [["B", 1]],
-                   "B": [["C", 3], ["D", 2], ["E", 1]],
+                   "B": [["C", 3], ["D", 2],  ["E", 1]],
                    "C": [["D", 1], ["E", 4]],
                    "D": [["A", 2], ["E", 2]],
                    "E": [["F", 3]],
-                   "G": [["D", 1]],
-                   "F": None}
+                   "G": [["D", 1]],}
 
 
 def calcDistance(route: list):
-    distance = 0
-    if isRoute(route):
-        r = [route[0]]
-        lastStation = route[len(route) - 1]
-        for station in route:
-            # print(station)
+      distance = 0
+      route1 = [route[0]]
+      lastStation = route[len(route) - 1]
+      for station in route:
+            #print(type(station))
             if station == lastStation:
-                break
+                  break
             for next in stations_routes.get(station):
-                # print(next)
-                for x in next:
-                    if type(x) == str:
-                        if x == route[route.index(station) + 1]:
-                            # print(x)
-                            nxtStation, dist = next
-                            r.append(nxtStation)
-                            distance += dist
-        return f"\nTotal distance of route {r} is {distance}."
-    else:
-        return f"\nRoute {route} does not exist."
+                  #print(next)
+                  for x in next:
+                        if type(x) == str:
+                              if x == route[route.index(station) + 1]:
+                                    #print(x)
+                                    nxtStation, dist = next
+                                    route1.append(nxtStation)
+                                    distance += dist
+      if route1 == route:
+            return f"\nTotal distance of route {route1} is {distance}."
+      else:
+            return f"\nRoute {route} does not exist."
 
-
-def isRoute(route: list):
-    route1 = [route[0]]
-    lastStation = route[len(route) - 1]
-    for station in route:
-        #print(station)
-        if station == lastStation:
-            break
-        for next in stations_routes.get(station):
-            #print(next)
-            for x in next:
-                if type(x) == str:
-                    if x == route[route.index(station) + 1]:
-                        route1.append(x)
-    if route == route1:
-        return True
-    else:
-        return False
 
